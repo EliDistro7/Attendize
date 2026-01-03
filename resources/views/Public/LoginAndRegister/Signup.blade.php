36c6b4f472220a21772a963e29386bcb9776d704
elidistro7 <elidistro@gmail.com>
2026-01-03 07:35:12 +0300

    rote errors 2

--- resources/views/Public/LoginAndRegister/Signup.blade.php
+++ resources/views/Public/LoginAndRegister/Signup.blade.php
@@ -81,7 +81,7 @@
                     <p><input class="btn btn-block btn-success" type="submit" value="@lang('User.sign_up')"></p>
                 </div>
                 <div class="signup">
-                    <span>{!! @trans("User.already_have_account", ["url"=>route("login")]) !!}</span>
+                    <span>{!! trans("User.already_have_account", ["url"=>route("login")]) !!}</span>
                 </div>
             </div>
             {!! Form::close() !!}
