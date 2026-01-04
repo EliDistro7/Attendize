193ca46dea22d317cd7d8a0c55e76ca34051a200
elidistro7 <elidistro@gmail.com>
2026-01-04 09:11:02 +0300

    language 4

---
+++ resources/lang/sw/Order_Emails.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'received_new_order'             => 'Umepokea oda mpya kwa event',
+    'order_still_awaiting_payment'   => 'Tafadhali kumbuka: Oda hii bado inahitaji malipo.',
+    'manage_order'                   => 'Unaweza kudhibiti oda hii kwa',
+    'successful_order'               => 'Oda yako kwa event <strong>:name</strong> imefanikiwa.',
+    'tickets_attached'               => 'Tiketi zako zimeambatanishwa kwenye barua pepe hii. Unaweza pia kuangalia maelezo ya oda yako na kupakua tiketi zako kwa:',
+];
